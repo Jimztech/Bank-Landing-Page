b86f73ad5ff8fa0ee102c1c27ccc19ae6bfffe23
jimztech <ibezimjames20@gmail.com>
2025-01-06 17:09:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const main = document.querySelector(".main");
+const openMenu = document.querySelector(".open-menu");
+const navbar = document.querySelector(".nav-links");
+
+openMenu.addEventListener("click", () => {
+    if(navbar.style.display === "flex") {
+        navbar.style.display = "none";
+        openMenu.src = "./images/icon-hamburger.svg"
+    } else {
+        navbar.style.display = "flex";
+        openMenu.src = "./images/icon-close.svg"
+    }
+});
