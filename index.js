35d6d2e5481db9873ba4b8b3cfb0231689a092b8
jimztech <ibezimjames20@gmail.com>
2025-01-13 00:10:39 +0100

    updates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".main");
 const openMenu = document.querySelector(".open-menu");
 const navbar = document.querySelector(".nav-links");
+const requestBtn = document.querySelector(".btn");
+const body = document.body;
 
 openMenu.addEventListener("click", () => {
     if(navbar.style.display === "flex") {
@@ -17,4 +19,39 @@ main.addEventListener("click", () => {
         navbar.style.display = "none";
         openMenu.src = "./images/icon-hamburger.svg";
     }
+});
+
+// New Page powered by requestBtn.
+requestBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    body.innerHTML = `
+        <main id="second-main">
+            <div class="img-container">
+                <img src="./images/logo.svg" alt="logo">
+            </div>
+
+            <div class="form-design">
+                <form action="sign-up">
+                    <div class="input-wrapper">
+                        <span class="error-message" id="fname-error"></span>
+                        <input type="text" id="fname" placeholder="Enter Your Name" required>
+                    </div>
+
+                    <div class="input-wrapper">
+                        <span class="error-message" id="email-error"></span>
+                        <input type="email" name="email-address" id="email" placeholder="Email" required>
+                    </div>
+
+                    <div class="input-wrapper">
+                        <input type="number" name="age" placeholder="age" required>
+                    </div>
+
+                    <div class="submit-wrapper">
+                        <input type="submit" id="submit" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </main>
+    `
 })
