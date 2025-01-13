df60124037354f025652d144cc0035ba0df4b8e1
jimztech <ibezimjames20@gmail.com>
2025-01-13 16:20:59 +0100

    updated js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const main = document.querySelector(".main");
 const openMenu = document.querySelector(".open-menu");
 const navbar = document.querySelector(".nav-links");
 const requestBtns = document.querySelectorAll(".btn");
-const userName = document.getElementById("fname");
-const emailAddress = document.getElementById("email");
-const submitBtn = document.getElementById("submit");
 const body = document.body;
 
 openMenu.addEventListener("click", () => {
@@ -40,15 +37,16 @@ function loadSecondPage() {
             </div>
 
             <div class="form-design">
-                <form action="sign-up">
+                <form action="contact-form">
+                    <input type="hidden" name="contact_number" value="697483">
                     <div class="input-wrapper">
                         <span class="error-message" id="fname-error"></span>
-                        <input type="text" id="fname" placeholder="Enter Your Name" required>
+                        <input type="text" name="user_name" id="fname" placeholder="Enter Your Name" required>
                     </div>
 
                     <div class="input-wrapper">
                         <span class="error-message" id="email-error"></span>
-                        <input type="email" name="email-address" id="email" placeholder="Email" required>
+                        <input type="email" name="user_email" id="email" placeholder="Email" required>
                     </div>
 
                     <div class="input-wrapper">
@@ -62,7 +60,86 @@ function loadSecondPage() {
             </div>
         </main>
     `
+
+    // Third page powered by the submitBtn.
+    const userName = document.getElementById("fname");
+    const emailAddress = document.getElementById("email");
+    const submitBtn = document.getElementById("submit");
+
+    submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+    
+        let isValid = true;
+
+        // Validating Name.
+        if(userName.value.trim() === "") {
+            showError("fname-error", "Name cannot be empty!");
+            isValid = false;
+        } else {
+            hideError("fname-error");
+        }
+    
+        // Validating Email.
+        if(!validateEmail(emailAddress.value)) {
+            showError("email-error", "Looks like this is not an email");
+            isValid = false;
+        } else {
+            hideError("email-error");
+        }
+    
+        // Validating all fields
+        if(isValid) {
+            loadThirdPage();    
+        } else {
+            submitBtn.classList.add("disabled");
+        }
+    });
+
+    function loadThirdPage() {
+        body.innerHTML = `
+            <div class="successful-page">
+                <h1>Welcome to Easybank</h1>
+                <p>
+                    Thank you for signing up <strong>${userName.value.trim()}</strong><br>
+                    An email has been sent to <strong>${emailAddress.value.trim()}</strong>
+                </p>
+            </div>
+        `
+    }
 }
 
 
-// Third page powered by 
+function showError(id, message) {
+    const errorElement = document.getElementById(id);
+    errorElement.textContent = message;
+    errorElement.style.display = "block";
+}
+
+function hideError(id) {
+    const errorElement = document.getElementById(id);
+    errorElement.style.display = "none";
+}
+
+function validateEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+
+// EmailJs functionalities
+emailjs.init({
+    publickey: "rvnovK00iJVgvpzlJ",
+});
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+};
